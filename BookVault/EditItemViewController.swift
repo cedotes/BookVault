a37c7395fd3503333f8fe8bd7add470ebf799e47
Caro <caroline.rausch@tu-dresden.de>
2015-01-18 13:46:21 +0100

    new EditView + EditViewController
    + updated constrains
    Work packages file added

---
+++ BookVault/EditItemViewController.swift
@@ -0,0 +1,45 @@
+//
+//  EditItemViewController.swift
+//  BookVault
+//
+//  Created by Caroline on 18.01.15.
+//  Copyright (c) 2015 David Gollasch, Caroline Rausch. All rights reserved.
+//
+
+import UIKit
+
+class EditItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showFurtherInformation(sender: UIButton) {
+        let alertController = UIAlertController(title: "Alert", message:
+            "clicked show", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
